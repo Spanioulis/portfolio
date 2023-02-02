97f7f814da64caaa2345a21b495039b2c943d2fe
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-02-02 11:58:29 +0100

    Scrollbar desgin
    Aplica estilos a scrollbar (Y)

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -11,6 +11,24 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     overflow-x: hidden;
+
+    ::-webkit-scrollbar {
+        width: 13px;
+    }
+
+    ::-webkit-scrollbar-track {
+        -webkit-box-shadow: inset 0 0 6px rgb(68, 68, 68, 0.3); 
+        border-radius: 5px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        border-radius: 5px;
+        -webkit-box-shadow: inset 0 0 6px rgb(115, 115, 115, 0.6)
+    }
+        ::-webkit-scrollbar-thumb:hover {
+        border-radius: 5px;
+        -webkit-box-shadow: inset 0 0 6px rgb(115, 115, 115, 0.8)
+    }
   }
 
   /* Contenedor para las dos imágenes */
